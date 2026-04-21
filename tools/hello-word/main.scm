14033b64e164cabd99bb430bad7f61870676a805
weekend-project-space <sunxzg@gmail.com>
2026-04-21 09:34:39 +0800

    hello word

---
+++ tools/hello-word/main.scm
@@ -0,0 +1,39 @@
+#| @meta
+{
+  "name": "hello-word",
+  "description": "返回一个简单的问候语，适合验证 Scheme tool 是否工作正常",
+  "args": [
+    {
+      "name": "name",
+      "type": "string",
+      "required": false,
+      "default": "world",
+      "description": "可选的人名或目标词，默认 world"
+    }
+  ],
+  "returns": {
+    "type": "string",
+    "description": "hello <name> 格式的问候语"
+  },
+  "examples": [
+    "openwalk run hello-word",
+    "openwalk run hello-word -- OpenWalk"
+  ],
+  "domains": [],
+  "readOnly": true,
+  "requiresLogin": false,
+  "tags": [
+    "hello",
+    "demo",
+    "smoke-test"
+  ]
+}
+|#
+
+(define (main args)
+  (define target
+    (if (null? args)
+        "world"
+        (car args)))
+  (string-append "hello " target))
+
